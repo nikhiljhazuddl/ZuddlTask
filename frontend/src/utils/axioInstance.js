8c1b28083954942fe0b8d0d373220fb2fa755be9
Nikhil Jha <nikhilaryan2021@gmail.com>
2025-10-30 17:01:27 +0530

    Fix API URL - add /api suffix

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://zuddltask.onrender.com"
+const BASE_URL = "https://zuddltask.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
